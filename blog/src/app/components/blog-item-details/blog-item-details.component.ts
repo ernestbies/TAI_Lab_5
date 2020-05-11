9b98a0769aeef23cb58b1e4a30d618f10907dcb8
ernestbies <ernestbies@gmail.com>
2020-05-11 23:03:31 +0200

    posts on blog page are now from database

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -25,8 +25,8 @@ export class BlogItemDetailsComponent implements OnInit {
       });
 
       this.dataService.getById(id).subscribe(res => {
-        this.image = res['image'];
-        this.text = res['text'];
+        this.image = res['url'];
+        this.text = res['content'];
         this.title = res['title'];
       });
   }
